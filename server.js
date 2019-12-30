4dbfd8713f1c2df066ac18e5016d3d2e9d110a4a
krk chaitanya <krkchaitanya42@gmail.com>
2019-12-29 17:31:19 -0700

    added examples 1-2

--- server.js
+++ server.js
@@ -18,6 +18,46 @@ regexServer.get("/", (req, res) => {
 });
 
 
+// Example-1
+// find and replce the words of a sentence
+regexServer.get("/example1", (req, res) => {
+    const story = "There was once an employee named Robinson. Robinsn was not very smart, but she was loyal. I could have promoted robinson but i did not";
+    const nameSearchRegex = /Robinso?n/gi
+    const updatedStory = story.replace(nameSearchRegex, 'Griffin');
+    res.json({
+        title: "Example-1",
+        story: story,
+        updatedStory: updatedStory
+    });
+});
+
+
+// Example-2
+// Easy search and groups
+// regex format:  sandy: =>  [a-z]:
+// space: \s
+// count: {1,9} 0r +
+// gi: global caseinsensitive
+regexServer.get("/example2", (req, res) => {
+    
+    const regex = /([a-z\s]+):\s([a-z]+),\s([a-z]+)/i
+    
+    const vehiclesInfo = [
+        "Honda : Accord, CRV",
+        "Toyota : Camry, rav4",
+        "subaru : crosstrek, outback"
+    ];
+
+   const updatedVehiclesInfo = vehiclesInfo.map(vehicle => vehicle.replace(regex, '$3 $2 => $1'));
+
+    res.json({
+        title: "Example-2",
+        vehiclesInfo: vehiclesInfo,
+        updatedVehiclesInfo: updatedVehiclesInfo
+    });
+});
+
+
 // run the application on port 7277
 const port = 7277;
 regexServer.listen(port, () => console.log("regexServer is running on port :: " + port));
