8f23816eeaf8550d70a3d3c7d020112c485f905a
krk chaitanya <krkchaitanya42@gmail.com>
2020-01-01 17:30:56 -0700

    js regular expressions continued

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ regexServer.use(bodyParser.urlencoded({extended: false}));
 regexServer.get("/", (req, res) => {
     res.json({
         title: "Default route of regexServer",
-        description: "Gives details and examples of javascript regular expressions"
+        description: "Regular expressions are nothing but a search pattern. We look for existence of certain pattern in a string"
     });
 });
 
@@ -56,6 +56,125 @@ regexServer.get("/example2", (req, res) => {
         updatedVehiclesInfo: updatedVehiclesInfo
     });
 });
+
+
+// test method 
+regexServer.get("/regex/testMethod", (req, res) => {
+   let testSentence = "The dod chased the cat";
+   let testRegex = /chased/;
+   let result = testRegex.test(testSentence);
+   res.json({
+       description: "test() method: validates the testSentence aginst the testRegex. If the regex value is found in the testSentence string it return boolean value true",
+       testSentence: testSentence,
+       testRegex: testRegex.toString(),
+       result: result
+   }); 
+});
+
+// match literal strings
+regexServer.get("/regex/matchLiteralStrings", (req, res) => {
+    let testSentence = "Lost In Space";
+    let testRegex = /space/;
+    let testRegexWithCaseInsensitive = /space/i;
+    let result = testRegex.test(testSentence);
+    let result1 = testRegexWithCaseInsensitive.test(testSentence);
+    res.json({
+        testSentence: testSentence,
+        testRegex: testRegex.toString(),
+        testRegexWithCaseInsensitive: testRegexWithCaseInsensitive.toString(),
+        caseSensitive: result,
+        caseInSensitive: result1
+
+    }); 
+ });
+
+
+//  Matching a literal string from multiple options
+ regexServer.get("/regex/matchLiteralStringFromMultipleOptions", (req, res) => {
+     let testSentence = "Judy, Penny and Will are the children of robinsons";
+     let testRegex = /vijay|judy|clarke|will/i;
+     let result = testRegex.test(testSentence);
+     res.json({
+         testSentence: testSentence,
+         testRegex: testRegex.toString(),
+         result: result
+     });
+});
+
+
+// match method
+regexServer.get("/regex/extractMatches", (req, res) => {
+    let testSentence = "Judy, Penny and Will are the children of robinsons";
+    let testRegex = /will/i;
+    let result = testSentence.match(testRegex);
+    res.json({
+        description: "Extract single matche",
+        testSentence: testSentence,
+        testRegex: testRegex.toString(),
+        result: result
+    });
+});
+
+// extract multiple matches
+regexServer.get("/regex/extractMultpleMatches", (req, res) => {
+    let testSentence = "Judy, Penny and Will will are the children of robinsons";
+    let testRegex = /will|calrke|penny/gi;
+    let result = testSentence.match(testRegex);
+    res.json({
+        description: "Extract multiple matches",
+        testSentence: testSentence,
+        testRegex: testRegex.toString(),
+        result: result
+    });
+});
+
+
+// using dot or wildcard period
+regexServer.get("/regex/dorOrPeriod", (req, res) => {
+    let testSentence = "Judy, Penny and Will will are the children of robinsons";
+    let testRegex = /wil.|calrke|pen../gi;
+    let result = testSentence.match(testRegex);
+    res.json({
+        description: "Wilcard period or dot ",
+        testSentence: testSentence,
+        testRegex: testRegex.toString(),
+        result: result
+    });
+});
+
+// single character multiple options
+regexServer.get("/regex/singleCharacterMultipleOptions", (req, res) => {
+    let testSentence = "Judy, Penny and Will will are the children of robinsons";
+    let testSentence1 = "space spade sport";
+    const testRegex = /ju[aeiou]y|penn[aiu]|w[aeiou]ll/gi;
+    const testRegex1 = /sp[aei][cdio]e|spo../gi;
+    let result = testSentence.match(testRegex);
+    let result1 = testSentence1.match(testRegex1);
+    res.json({
+        description: "single character multiple options ",
+        testSentence: testSentence,
+        testRegex: testRegex.toString(),
+        testSentence1: testSentence1,
+        testRegex1: testRegex1.toString(),
+        result: result,
+        result1: result1
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // run the application on port 7277
